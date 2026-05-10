eeead8d1c7788f7d444c321c157092aa7636381e
sungwwo <sungwwo72@gmail.com>
2026-05-11 01:24:42 +1000

    Fix route and function for property details view

--- app/details/views.py
+++ app/details/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_temlplate, flash, url_for
 from . import details.bp
 
-@details_bp.route("/details/<property_id>")
-def propety_details(property_id):
-
-  return render_template('pages/details.html', property_id=property_id)
+@details_bp.route("/details")
+def details():
+  return render_template('pages/details.html')
