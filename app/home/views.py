e39691401d76c81cd63397443930f1cb98fda72c
Lianne <lianne416.nju@gmail.com>
2026-05-13 15:29:37 +1000

    error page done

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,3 @@
-from flask import render_template, request, session, flash
+from flask import Blueprint, render_template, request, session, flash
 from flask import redirect, url_for
 
-@home.route('/')
-def index():
-    return render_template('index.html')
