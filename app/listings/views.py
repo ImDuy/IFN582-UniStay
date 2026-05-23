5e27629a8b8101455dee9627d3a4e361d78e8b58
ImDuy <158559509+ImDuy@users.noreply.github.com>
2026-05-23 19:47:10 +1000

    add custom decorators for session-based role checks

--- app/listings/views.py
+++ app/listings/views.py
@@ -5,14 +5,15 @@ from app.listings.forms import PropertyForm
 from app.models import Property
 from app.constants import UserRole
 from app.db import add_property, delete_property, get_properties_by_agent, get_all_properties, update_property
+from app.wrappers import role_required
 from . import listings_bp
 
 
 @listings_bp.route('/')
+# @role_required([UserRole.AGENT.value, UserRole.ADMIN.value])
 def index():
     # at this stage, it should be guaranteed that the user logged in as agent or admin (the home page should only show the option to navigate to listings page if the user is agent or admin)
 
-    # get user id from session
     user_id = session.get('user_id') # need this id for query database
     properties: List[Property] = []
     form = PropertyForm() # form to render on modal
@@ -24,21 +25,19 @@ def index():
         # for now just render the listings page for testing (will be replace with the above return)
         return render_template('/pages/listings.html', listings = get_all_properties(), form=form, PropertyForm = PropertyForm)
 
-    # check user role
-    user_role = session.get('role') 
-    if user_role == UserRole.TENANT.value:
-        # if user is tenant, redirect to home page
-        return redirect(url_for('home.index'))
+    # check the user's role to render the corresponding properties.
+    user_role = session['user']['user_role']
     if user_role == UserRole.AGENT.value:
         # query in database to get all properties managed by this agent
-        properties = get_properties_by_agent(user_id)
+        properties = get_properties_by_agent(session['user']['user_id'])
     elif user_role == UserRole.ADMIN.value:
         # query in database to get all properties existing in the application
         properties = get_all_properties()
 
-    return render_template('/pages/listings.html', listings = properties, form=form)
+    return render_template('/pages/listings.html', listings = properties, form=form, PropertyForm = PropertyForm)
 
 @listings_bp.post('/')
+# @role_required([UserRole.AGENT.value])
 def create_property_listing():
     # admin is not able to create property listing, so in the UI, the button of adding property should not display when the user logged in as admin
 
@@ -66,6 +65,7 @@ def create_property_listing():
     return redirect(url_for('listings.index'))
 
 @listings_bp.post('/<string:property_id>/edit')
+# @role_required([UserRole.AGENT.value, UserRole.ADMIN.value])
 def edit_property_listing(property_id):
     # only admin and agent are allowed to perform edit action
     # user_role = session.get('user_role')
@@ -84,6 +84,7 @@ def edit_property_listing(property_id):
     return redirect(url_for('listings.index'))
 
 @listings_bp.route('/<string:property_id>/delete')
+# @role_required([UserRole.AGENT.value, UserRole.ADMIN.value])
 def delete_property_listing(property_id):
     # only admin and agent are allowed to perform delete action
     # user_role = session.get('user_role')
