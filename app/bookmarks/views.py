fae7d53037b108d8faff250b180ea27ef558795c
henrychan <henry7410@gmail.com>
2026-05-27 16:30:09 +0800

    change the navbar

--- app/bookmarks/views.py
+++ app/bookmarks/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, flash
+from flask import render_template, session, flash
 from flask import redirect, url_for
 from app.constants import UserRole
 from . import bookmarks_bp
@@ -11,18 +11,8 @@ from app.wrappers import role_required
 def index():
     # query user's bookmarks list default using the userId and check the role if it's not tenant than redirect to homepage
     user_id = session['user']['user_id']
-    if not user_id:
-        #if user didn't log in redirect to login page
-        return redirect(url_for('home.index'))
-    
-    user_role = session['user']['user_role']
-    print('user_role >', user_role)
-    if user_role == UserRole.TENANT.value:
-        # get tenant's bookmark list and get the properties in the list
-        bookmarks = get_bookmark_by_tenant(user_id)
-        print('bookmarks', bookmarks)
-    else:
-        return redirect(url_for('home.index'))
+    bookmarks = get_bookmark_by_tenant(user_id)
+
     return render_template('/pages/bookmarks.html', bookmarks=bookmarks)
 
 # redirect to bookmarks page when delete bookmarks
