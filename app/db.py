725f01947953917d9422abf2e67b4dfb0afa305c
sungwwo <sungwwo72@gmail.com>
2026-06-04 16:53:38 +1000

    Add function to get property documents by ID
    Added a new function to retrieve documents for a property.

--- app/db.py
+++ app/db.py
@@ -298,6 +298,13 @@ def get_uni_nearby(property_id):
     cursor.close()
     return nearby_universities
 
+def get_documents(property_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT url FROM propertydocumentation WHERE propertyId = %s", (property_id,))
+    documents = cursor.fetchall()
+    cursor.close()
+    return documents
+
 def bookmark_overlap(tenant_id, property_id):
     cursor = mysql.connect.cursor()
     cursor.execute("SELECT propertyId FROM bookmark WHERE tenantId = %s AND propertyId = %s ", (tenant_id, property_id))
